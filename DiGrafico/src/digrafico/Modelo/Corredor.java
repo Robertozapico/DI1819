f208c42b9f451c2c8da2b6daf2aa22c6d1ef7388
robertozapicoblanco@gmail.com <robertozapicoblanco@gmail.com>
2018-10-01 10:33:31 +0200

    Añadidas cosas en el proyecto

---
+++ DiGrafico/src/digrafico/Modelo/Corredor.java
@@ -0,0 +1,125 @@
+package digrafico.Modelo;
+
+
+
+import java.util.Date;
+import java.util.Objects;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumnop
+ */
+public class Corredor implements Comparable<Corredor>{
+    private String nombre;
+    private String dni;
+    private Date fechaNacimiento;
+    private String direccion;
+    private int telefono;
+
+    public Corredor() {
+    }
+    
+
+    public Corredor(String nombre, String dni, Date fechaNacimiento, String direccion, int telefono) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+    
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Corredor{" + "nombre=" + nombre + ", dni=" + dni + ", fechaNacimiento=" + fechaNacimiento + ", direccion=" + direccion + ", telefono=" + telefono + '}';
+    }
+
+    @Override
+    public int compareTo(Corredor o) {
+        return this.dni.compareTo(o.dni);
+    }
+    
+    public int compareToFechaNac(Corredor o) {
+        return this.fechaNacimiento.compareTo(o.fechaNacimiento);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.dni);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Corredor other = (Corredor) obj;
+        if (!Objects.equals(this.dni, other.dni)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+}
