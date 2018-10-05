167d8eb799e8ca8b05a95bf6ba9b4a01cc31f9e2
robertozapicoblanco@gmail.com <robertozapicoblanco@gmail.com>
2018-10-05 11:31:07 +0200

    Ejercicio AD

---
+++ AcUt02/src/acut02/Modelo/OperacionesFicheros.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acut02.Modelo;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alumnop
+ */
+public class OperacionesFicheros {
+//http://bit.ly/JJCMAD2018
+
+    private File[] rutasFicheros;
+
+    //Ejercicio 1.A
+    public File[] ListarFicheros(String ruta, boolean ordenadosPorTamanio, boolean soloDirectorios) {
+        File[] lista = null;
+        File archivos = new File(ruta);
+
+        if (ruta.equals("")) {
+            rutasFicheros = File.listRoots();
+        }
+
+        lista = archivos.listFiles();
+        //HACER ESTAS EXCEPCIONES COMO PROPIAS
+        if (archivos.isDirectory() == false) {
+            try {
+                throw new ExcepcionPersonalizada/*IllegalArgumentException*/("La ruta no es un directorio, no se puede listar");
+            } catch (ExcepcionPersonalizada ex) {
+                String respuesta = ex.getMessage();
+            }
+        }
+        if (lista.length == 0) {
+            try {
+                throw new ExcepcionPersonalizada/*IllegalArgumentException*/("La carpeta está vacia");
+            } catch (ExcepcionPersonalizada ex) {
+                String respuesta = ex.getMessage();
+            }
+            /*throw new IllegalArgumentException("La carpeta está vacia");*/
+        }
+        if (ordenadosPorTamanio == true) {
+
+            Arrays.sort(lista, new Comparator<File>() {
+                @Override
+                public int compare(File fichero1, File fichero2) {
+                    if (fichero1.length() < fichero2.length()) {
+                        return 1;
+                    } else if (fichero1.length() > fichero2.length()) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+
+            });
+            /*
+            for (int i = 0; i < lista.length; i++) {
+                System.out.println(lista[i].toString());
+                System.out.println(lista[i].length());
+
+            }
+             */
+        }
+        /*else {
+            for (int i = 0; i < lista.length; i++) {
+                System.out.println(lista[i].toString());
+                System.out.println(lista[i].length());
+
+            }
+        }*/
+
+        if (soloDirectorios == true) {
+
+            for (int i = 0; i < lista.length; i++) {
+                if (lista[i].isDirectory() == true) {
+                    System.out.println(lista[i].toString());
+                    System.out.println(lista[i].length());
+                }
+            }
+        } else {
+            for (int i = 0; i < lista.length; i++) {
+
+                System.out.println(lista[i].toString());
+                System.out.println(lista[i].length());
+            }
+        }
+
+        /* 
+iv.Crear dos  excepciones personalizadas  
+        Carpeta Vacia y NoEsUnDirectorioNoSePuedeListar v Probar el correcto funcionamiento desde un método main().Generar Javadoc.
+         */
+        return lista;
+    }
+
+    public int crearDirectorio(File rutaOrigen, ArrayList<String> listaDirectorios) {
+        int contadorCreados = 0;
+        if (rutaOrigen.exists() == false) {
+            throw new IllegalArgumentException("La ruta no existe");
+        }
+        for (String listaDirectorio : listaDirectorios) {
+            File nuevoDirectorio = new File(rutaOrigen + "/" + listaDirectorio.toString());
+            if (nuevoDirectorio.exists()) {
+                throw new IllegalArgumentException("El directorio " + listaDirectorio + " ya existe");
+            }
+            nuevoDirectorio.mkdir();
+            contadorCreados++;
+
+        }
+
+        return contadorCreados;
+    }
+
+    //Ejercicio 2
+    public int fibonacci(int n) {
+        if (n > 1) {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 0;
+        } else {
+            System.out.println("Debes introducir un dato superior o igual a 1");
+            return -1;
+        }
+    }
+    //Factorial  
+
+    //Recursividad
+    public void listarDirectorio(String ruta) {
+        File rutaDirectorio = new File(ruta);
+        File[] ficheros = null;
+        ficheros = rutaDirectorio.listFiles();
+        int x;
+        for (x = 0; x < ficheros.length; x++) {
+            System.out.println(" " + ficheros[x].getName());
+        }
+        if (ficheros[x].isDirectory()) {
+            String nuevo_separador;
+            nuevo_separador = " ";
+            listarDirectorio(ruta + ficheros[x]);
+        }
+        //return rutaDirectorio;
+    }
+
+    public void listarArchivosRecursivamente(String ruta) {
+        File rutaFicheros = new File(ruta);
+        File[] ficheros = rutaFicheros.listFiles();
+
+        for (int i = 0; i < ficheros.length; i++) {
+
+            if (ficheros[i].isDirectory() == false) {
+                String guion = "-";
+                for (int j = 0; j < ficheros[i].getParent().length(); j++) {
+                    guion += "-";
+                }
+                System.out.println("\n" + "./" + guion + "/" + ficheros[i].getName());
+
+            } else {
+
+                System.out.println(ficheros[i].getAbsolutePath());
+                listarArchivosRecursivamente(ficheros[i].getAbsolutePath());
+
+            }
+
+        }
+    }
+
+    public ArrayList<File>  listarFicheros(FilenameFilter  filtro){
+        
+        return null;
+    }
+    
+    /*Crear  los  siguientes  filtros  implementado  la  interface  FileNameFilter  (Se  puede  crear  una  clase  Filtros):  
+    i.Filtro  ficheros  de  imágenes  (jpg,  gif,  tiff,  ...)  
+    ii.Filtro  ficheros  de  video  (avi,  mp4,  mkv,  ...)  
+    iii.Filtro  directorios.    iv.Filtro  ficheros  con  un  tamaño  mínimo.  
+    v.Filtro  fichero  modificados  en  las  últimas  24  horas.    
+    vi.Probar  el  correcto  funcionamiento  desde  un  método  main().  
+    vii.Generar  Javadoc. */
+    
+}
---
+++ AcUt02/src/acut02/Modelo/main.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acut02.Modelo;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alumnop
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] lista = null;
+        File[] archivos = null;
+        OperacionesFicheros of = new OperacionesFicheros();
+        //prueba de que el ejercicio 1.A funciona entero, falta javadoc
+        //of.ListarFicheros("/home/alumnop/Carpetavacia/docu.txt", true, true);
+
+        //prueba de que el ejercicio 1.B funciona entero, falta javadoc
+        /*ArrayList<String> listaDirectorios = new ArrayList<String>();
+        
+        listaDirectorios.add("Nueva");
+        listaDirectorios.add("Vacia");
+        listaDirectorios.add("Prueba");
+        File ruta = new File("/home/alumnop/Carpetavacia");
+        System.out.println(of.crearDirectorio(ruta, listaDirectorios));
+         */
+ /*FIBONACCI
+        for (int i = 0; i < 10; i++) {
+            System.out.print(of.fibonacci(i) + " ");
+        }
+         */
+        
+         of.listarArchivosRecursivamente("/home/alumnop/Carpetavacia");
+
+    }
+
+}
